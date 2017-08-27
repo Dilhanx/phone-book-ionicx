3c5e91e695e2021cdce5bd70beadad5b1fe4799f
Dilhanx <dilhanx@outlook.com>
2017-08-27 21:45:29 +0530

    Added List page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { FormPage } from "../form/form";
+import { ListPage } from "../list/list";
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  title =  "Phone Book";
   constructor(public navCtrl: NavController) {
 
   }
-
+  goToFormPage() {
+    this.navCtrl.push(FormPage);
+  }
+  goToListPage() {
+    this.navCtrl.push(ListPage);
+  }
 }
---
+++ src/pages/list/list.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http,RequestOptions } from "@angular/http";
+import 'rxjs/Rx';
+/**
+ * Generated class for the ListPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html',
+})
+export class ListPage {
+  list=[]
+  constructor(public navCtrl: NavController, public navParams: NavParams,private htpp:Http) {
+  }
+  
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ListPage');
+    this.htpp.get("https://phone-book-sailsx.herokuapp.com/contact").map(res=> res.json()) .subscribe(data =>{ this.list=data});
+  }
+
+}
