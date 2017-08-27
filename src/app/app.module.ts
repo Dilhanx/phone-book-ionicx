94aae36f5ae38818a89308d6f070cde4c80f02c0
Dilhanx <dilhanx@outlook.com>
2017-08-27 23:26:26 +0530

    Added Tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,33 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { FormPage } from '../pages/form/form';
 import { ListPage } from '../pages/list/list';
-
+import { TabsPage } from '../pages/tabs/tabs';
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     FormPage,
-    ListPage
+    ListPage,
+    TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
     FormPage,
-    ListPage
+    ListPage,
+    TabsPage
   ],
   providers: [
     StatusBar,
